d110c8390b33b717c931a50e4594d265ebb47435
ZIUTY-TTPro <tomasz.zietkowski@gmail.com>
2026-01-16 14:10:21 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'turniej-v2';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
